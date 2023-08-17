b194a78ed26dc86dcd26b79cebd4927408e1ed3b
molikuner <molikuner@gmail.com>
2023-08-17 23:57:00 +0200

    Create conventions plugin

---
+++ buildSrc/src/main/kotlin/pure-kotlin-ios.app-conventions.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+group = "com.molikuner"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    ios {
+        binaries {
+            executable {
+                entryPoint = "com.molikuner.kn.ios.main"
+            }
+        }
+    }
+    iosSimulatorArm64 {
+        binaries {
+            executable {
+                entryPoint = "com.molikuner.kn.ios.main"
+            }
+        }
+    }
+    sourceSets {
+        val iosMain by getting
+        val iosTest by getting
+        val iosSimulatorArm64Main by getting {
+            dependsOn(iosMain)
+        }
+        val iosSimulatorArm64Test by getting {
+            dependsOn(iosTest)
+        }
+
+        all {
+            languageSettings {
+                optIn("kotlinx.cinterop.BetaInteropApi")
+                optIn("kotlinx.cinterop.ExperimentalForeignApi")
+            }
+        }
+    }
+}
