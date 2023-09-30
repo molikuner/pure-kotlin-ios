460b6041b880217f267fbf9af21f5bc1398ab362
molikuner <molikuner@gmail.com>
2023-10-01 01:19:56 +0200

    Update Kotlin to 1.9.10
    * Updated Compose to 1.5.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,10 +4,9 @@ plugins {
 
 repositories {
     gradlePluginPortal()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.5.0-rc01")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.5.2")
 }
