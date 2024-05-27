e08763d12af1901c8aad3198e02bb4d37f2c997c
molikuner <molikuner@gmail.com>
2024-05-27 14:44:52 +0200

    Upgrade to Kotlin 2.0.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0-RC2")
-    implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.0.0-RC2")
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.6.10-beta03")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+    implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.0.0")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.6.10")
 }
