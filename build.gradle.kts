1b8e1c71c10ba12b58361f56d11525fefe33e126
molikuner <molikuner@gmail.com>
2024-06-11 20:59:07 +0200

    Apply full workaround for KT-67968
    While I personally think not all of it should be neccessary, just for
    the sake of making the point that it's still broken, I included all
    convention plugins in the root project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
 plugins {
-    id("pure-kotlin-ios.compose-conventions") apply false
+    id("pure-kotlin-ios.app-conventions").apply(false)
+    id("pure-kotlin-ios.compose-conventions").apply(false)
+    id("pure-kotlin-ios.ios-conventions").apply(false)
 }
